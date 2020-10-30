6293b53e414022621287f37329877f1b3755fbda
carlosmuriloifsp <murilo.c@aluno.ifsp.edu.br>
2020-10-30 13:55:05 -0300

    Configuração inicial do sistema

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/lp2/murilo/';
+$config['base_url'] = 'localhost/lp2/murilo/';
 
 /*
 |--------------------------------------------------------------------------
