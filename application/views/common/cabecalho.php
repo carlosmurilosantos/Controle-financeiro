4091c1d8719715fcd3bd2853119729e6f932d863
carlosmuriloifsp <murilo.c@aluno.ifsp.edu.br>
2020-11-01 19:24:31 -0300

    Configuração dos assets do MDB

---
+++ application/views/common/cabecalho.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+            <meta http-equiv="x-ua-compatible" content="ie=edge">
+            <title>LPII - 2020/2</title>
+            
+            <link rel="icon" href="img/mdb-favicon.ico" type="image/x-icon">
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+            <link rel="stylesheet" href="<?= base_url() ?>assets/mdb/css/bootstrap.min.css">
+            <link rel="stylesheet" href="<?= base_url() ?>assets/mdb/css/mdb.min.css">
+            <link rel="stylesheet" href="<?= base_url() ?>assets/mdb/css/style.css">
+        </head>
+<body>
---
+++ application/views/common/rodape.php
@@ -0,0 +1,8 @@
+ 
+                <script type="text/javascript" src="<?= base_url() ?>assets/mdb/js/jquery.min.js"></script>
+                <script type="text/javascript" src="<?= base_url() ?>assets/mdb/js/popper.min.js"></script>
+                <script type="text/javascript" src="<?= base_url() ?>assets/mdb/js/bootstrap.min.js"></script>
+                <script type="text/javascript" src="<?= base_url() ?>assets/mdb/js/mdb.min.js"></script>
+                <script type="text/javascript"></script>
+        </body>
+</html>
